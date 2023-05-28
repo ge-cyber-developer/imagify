f2ecd72cb02745047b7c9ba55b29069f1bf61d30
ge-cyber-developer <61585687+ge-cyber-developer@users.noreply.github.com>
2023-05-28 16:54:22 -0300

    Initial commit

---
+++ src/test/java/com/imagegenerator/imagegenerator/ImagegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imagegenerator.imagegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagegeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
